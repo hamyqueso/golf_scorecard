14bf1449143b35a173ab023582e04b1a108479b3
hamyqueso <jonathan.cheung09@gmail.com>
2025-06-04 10:06:46 -0700

    moved some functions from main to api_functions. started on some print statements displaying the scorecard

--- api_functions.py
+++ api_functions.py
@@ -30,3 +30,68 @@ def search_for_course(search_query):
 
     data = response.json()
     return data
+
+def display_course_choices():
+
+    # code to limit number of results displayed
+    # list_len = 0
+    # if len(search_results['courses']) <= 5:
+    #     list_len = len(search_results['courses'])
+    # else:
+    #     list_len = 5
+
+    while True:
+        search_query = input("What is the name of the course?: ")
+        search_results = search_for_course(search_query)
+
+        if search_results['courses']:
+            for i in range(len(search_results['courses'])):
+                try:
+                    print(f'{i+1}. {search_results['courses'][i]['course_name']}, {search_results['courses'][i]['location']['address']}')
+                except KeyError:
+                    pass
+            
+            return search_results
+        else:
+            print(f"No courses found with name '{search_query}'. Please enter course name again.")
+
+def choose_course(search_results):
+    
+
+    while True:
+        choice = input("Please enter the number of the course you are playing: ")
+        try:
+            choice = int(choice)
+            return search_results['courses'][choice - 1]
+        except TypeError:
+            print("Error: please enter a number")
+        except IndexError:
+            print("Error: choice not in range of the search results")
+
+def select_tees(course):
+    while True:
+        print("Genders:")
+        for gender in course['tees']:
+                print(gender)
+        gender_choice = input("Which gender tees are you playing?: ").lower()
+        try:
+            tee_gender = course['tees'][gender_choice]
+            print()
+            break
+        except KeyError:
+            print("Error: Enter a gender from the choices.")
+        
+        print()
+    while True:
+        print("Tees colors:")
+        for tee_color in tee_gender:
+            print(f"{tee_color['tee_name']}, total yards: {tee_color['total_yards']}, par total: {tee_color['par_total']}")
+
+        print()
+        tee_choice = input("Which tee color are you playing?: ").capitalize()        
+        for i in range(len(tee_gender)):
+            if tee_choice == tee_gender[i]["tee_name"]:
+                return course["course_name"], gender_choice, tee_gender[i]
+        
+        print("Error: invalid tee color, please re-enter your choice")
+        print()
--- main.py
+++ main.py
@@ -1,70 +1,29 @@
 from api_functions import *
 from os_functions import *
 
-def display_course_choices():
 
-    # code to limit number of results displayed
-    # list_len = 0
-    # if len(search_results['courses']) <= 5:
-    #     list_len = len(search_results['courses'])
-    # else:
-    #     list_len = 5
-
-    while True:
-        search_query = input("What is the name of the course?: ")
-        search_results = search_for_course(search_query)
-
-        if search_results['courses']:
-            for i in range(len(search_results['courses'])):
-                try:
-                    print(f'{i+1}. {search_results['courses'][i]['course_name']}, {search_results['courses'][i]['location']['address']}')
-                except KeyError:
-                    pass
-            
-            return search_results
-        else:
-            print(f"No courses found with name '{search_query}'. Please enter course name again.")
-
-def choose_course(search_results):
+def print_scorecard(course_name, gender, tee):
+    print(f"Course: {course_name}")
+    print(f"Gender: {gender}, Tees: {tee['tee_name']}")
+    holes = tee['holes']
     
-
-    while True:
-        choice = input("Please enter the number of the course you are playing: ")
-        try:
-            choice = int(choice)
-            return search_results['courses'][choice - 1]
-        except TypeError:
-            print("Error: please enter a number")
-        except IndexError:
-            print("Error: choice not in range of the search results")
-
-def select_tees(course):
-    while True:
-        print("Genders:")
-        for gender in course['tees']:
-                print(gender)
-        gender_choice = input("Which gender tees are you playing?: ").lower()
-        try:
-            tee_gender = course['tees'][gender_choice]
-            print()
-            break
-        except KeyError:
-            print("Error: Enter a gender from the choices.")
+    holes_string = "Hole "
+    par_string = "Par  "
+    yardage_string = "Yards "
+    handicap_string = "Handicap"
+    for i in range(len(holes)):
+        holes_string += f"   {i+1}   "
+        par_string += f"   {holes[i]["par"]}   "
+        yardage_string += f"  {holes[i]["yardage"]} "
+        handicap_string += f"   {holes[i]["handicap"]}   "
         
-        print()
-    while True:
-        print("Tees colors:")
-        for tee_color in tee_gender:
-            print(f"{tee_color['tee_name']}, total yards: {tee_color['total_yards']}, par total: {tee_color['par_total']}")
-
-        print()
-        tee_choice = input("Which tee color are you playing?: ").capitalize()        
-        for i in range(len(tee_gender)):
-            if tee_choice == tee_gender[i]["tee_name"]:
-                return course["course_name"], gender_choice, tee_gender[i]
-        
-        print("Error: invalid tee color, please re-enter your choice")
-        print()
+    # Instead of printing these from the function I think it would 
+    # be better to return the strings so we don't have to loop 
+    # through hole data everytime we print to the console 
+    print(holes_string)
+    print(par_string)
+    print(yardage_string)
+    print(handicap_string)
 
     
 
@@ -80,8 +39,12 @@ def main():
 
     search_results = display_course_choices()
     course = choose_course(search_results)
-    print(select_tees(course))
     clear_console()
+    course_name, gender, tees = select_tees(course)
+    while True:
+        clear_console()
+        print_scorecard(course_name, gender, tees)
+        score_update = input("What did you score on the hole?")
 
 
 if __name__ == "__main__":
