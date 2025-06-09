afb648bc69056df8c632e624371ebfa97390fd37
hamyqueso <jonathan.cheung09@gmail.com>
2025-06-09 07:13:07 -0700

    updated console print spacing for the scorecard header

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from api_functions import *
 from os_functions import *
+import math
 
 
 def print_scorecard(course_name, gender, tee):
@@ -7,25 +8,44 @@ def print_scorecard(course_name, gender, tee):
     print(f"Gender: {gender}, Tees: {tee['tee_name']}")
     holes = tee['holes']
     
-    holes_string = "Hole "
-    par_string = "Par  "
-    yardage_string = "Yards "
+    holes_string = "Hole    "
+    par_string = "Par     "
+    yardage_string = "Yards    "
     handicap_string = "Handicap"
     for i in range(len(holes)):
-        holes_string += f"   {i+1}   "
-        par_string += f"   {holes[i]["par"]}   "
-        yardage_string += f"  {holes[i]["yardage"]} "
-        handicap_string += f"   {holes[i]["handicap"]}   "
+        
+        holes_string += string_spacer(i+1)
+        par_string += string_spacer(holes[i]["par"])
+        yardage_string += string_spacer(holes[i]["yardage"])
+        handicap_string += string_spacer(holes[i]["handicap"])
+
+        # holes_string += f"   {i+1}   "
+        # par_string += f"   {holes[i]["par"]}   "
+        # yardage_string += f"  {holes[i]["yardage"]} "
+        # handicap_string += f"   {holes[i]["handicap"]}   "
         
     # Instead of printing these from the function I think it would 
     # be better to return the strings so we don't have to loop 
     # through hole data everytime we print to the console 
-    print(holes_string)
-    print(par_string)
-    print(yardage_string)
-    print(handicap_string)
+    return f"""Course: {course_name}
+Gender: {gender}, Tees: {tee['tee_name']}
+{holes_string}
+{par_string}
+{yardage_string}
+{handicap_string}"""
 
-    
+def string_spacer(s):
+    total_len = 7
+    s = str(s)
+    r_space = " " * math.floor((total_len - len(s))/2)
+    l_space = " " * math.ceil((total_len - len(s))/2)
+
+    result = l_space + s + r_space
+    return result
+
+def score_updater(holes, hole):
+    pass
+
 
 def main():
     # print("Hello World")
@@ -41,10 +61,14 @@ def main():
     course = choose_course(search_results)
     clear_console()
     course_name, gender, tees = select_tees(course)
+    scorecard_header = print_scorecard(course_name, gender, tees)
     while True:
         clear_console()
-        print_scorecard(course_name, gender, tees)
-        score_update = input("What did you score on the hole?")
+        print(scorecard_header)
+        try:
+            score_update = int(input("What did you score on the hole?"))
+        except TypeError:
+            print()
 
 
 if __name__ == "__main__":
